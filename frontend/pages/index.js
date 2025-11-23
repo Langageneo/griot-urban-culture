0d216a5ee754c9d794c3ab16b00c815c5c5cceb7
Parfait Guiri <parfait.guiri@gmail.com>
2025-11-23 05:44:39 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,22 +1,12 @@
-import { useState, useEffect } from 'react';
-import Feed from '../components/Feed';
-import HaloEffect from '../components/HaloEffect';
+import Logo from '../components/Logo/Logo';
+import ParticlesBackground from '../components/ParticlesBackground';
 
 export default function Home() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    // Récupérer les vidéos depuis le backend
-    fetch('http://localhost:5000/api/youtube/@scoopdurapivoirien6861')
-      .then(res => res.json())
-      .then(data => setVideos(data));
-  }, []);
-
   return (
-    <div className="container">
-      <HaloEffect />
-      <h1>Flux Rap Ivoirien</h1>
-      <Feed videos={videos} />
+    <div>
+      <ParticlesBackground />
+      <Logo animated={true} />
+      <h1>Griot Urban Culture</h1>
     </div>
   );
-    }
+}
