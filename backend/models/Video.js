ece4cc125b7eb7ad11612b7a5b607e5bbdd37bf0
Parfait Guiri <parfait.guiri@gmail.com>
2025-11-23 04:47:41 +0100

    Create Video.js

---
+++ backend/models/Video.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  publishedAt: { type: Date, required: true },
+  channelId: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Video', videoSchema);
